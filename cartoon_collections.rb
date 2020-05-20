e20e7ca7cc84cdf12f2f1cf274b4e34dc81e68ee
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 09:02:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,6 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
    array.find?{|element|
-  element.length > 4
+  valid_calls.include? element
   }
 end
