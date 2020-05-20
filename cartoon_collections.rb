404c36a096710a976bd1edabaa13b52c795b7c05
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 09:02:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-   array.map{|element|
+   planeteer_calls.map{|element|
   element.capitalize + "!"
   }
 end
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
   
-  array.any?{|element|
+  planeteer_calls.any?{|element|
   element.length > 4
   }
 end
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-   array.find?{|element|
+   planeteer_calls.find?{|element|
   valid_calls.include? element
   }
 end
